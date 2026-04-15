71e4054a4743e2f43e2f0526d842adf0bcd1d957
Sonali <86544294+Thotasonali@users.noreply.github.com>
2026-04-15 19:43:50 -0400

    Time: 8 ms (17.45%) | Memory: 45.6 MB (76.52%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x=0,y=0;
+        int n= moves.length();
+        for (int i=0;i<n;i++){
+            if(moves.charAt(i)=='R') x++;
+            else if(moves.charAt(i)=='L')x--;
+            else if(moves.charAt(i)=='U')y++;
+            else if(moves.charAt(i)=='D')y--;
+            else{
+                System.out.println("invalid")
+                ;
+            }
+        }
+        return x==0 && y==0;
+        
+    }
+}
