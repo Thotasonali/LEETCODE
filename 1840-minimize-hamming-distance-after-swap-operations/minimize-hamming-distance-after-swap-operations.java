dfa295d6deb4c29dbb7148aff4e27299e8b5e02f
Sonali <86544294+Thotasonali@users.noreply.github.com>
2026-04-21 11:10:47 -0400

    Time: 43 ms (84.95%) | Memory: 112 MB (89.25%) - LeetSync

---
+++ 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    public int minimumHammingDistance(int[] source, int[] target, int[][] allowedSwaps) {
+        int n = source.length;
+
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+
+        for (int[] swap : allowedSwaps) {
+            union(parent, swap[0], swap[1]);
+        }
+
+        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int root = find(parent, i);
+            map.putIfAbsent(root, new HashMap<>());
+            Map<Integer, Integer> freq = map.get(root);
+            freq.put(source[i], freq.getOrDefault(source[i], 0) + 1);
+        }
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            int root = find(parent, i);
+            Map<Integer, Integer> freq = map.get(root);
+
+            if (freq.getOrDefault(target[i], 0) > 0) {
+                freq.put(target[i], freq.get(target[i]) - 1);
+            } else {
+                res++;
+            }
+        }
+
+        return res;
+    }
+
+    private int find(int[] parent, int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent, parent[x]);
+        return parent[x];
+    }
+
+    private void union(int[] parent, int a, int b) {
+        int pa = find(parent, a);
+        int pb = find(parent, b);
+        if (pa != pb) parent[pa] = pb;
+    }
+}
