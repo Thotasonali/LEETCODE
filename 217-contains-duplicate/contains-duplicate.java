ead73b268992baf9f16a5b0e2fc03849b2f8a72b
Sonali <86544294+Thotasonali@users.noreply.github.com>
2026-06-18 11:59:56 -0400

    Time: 24 ms (22.79%) | Memory: 76.9 MB (84.41%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,4 +1,18 @@
+
 class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+/*class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> check=new HashSet<>();
         for(int num:nums ){
@@ -15,3 +29,4 @@ class Solution {
         
     }
 }
+*/
