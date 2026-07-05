4babef6aff5874e982d245caeb490a999332d521
Sonali <86544294+Thotasonali@users.noreply.github.com>
2026-07-05 17:38:57 -0400

    Time: 1 ms (99.96%) | Memory: 76.9 MB (95.89%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,17 +1,18 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-       
+        int max = nums[0];
         int sum = nums[0];
-        int current = nums[0];
-       
+        
         for (int i = 1; i < nums.length; i++) {
-           
-            current = Math.max(nums[i], current + nums[i]);
+            // Elimination of Math.max for current sum tracking
+            sum = (nums[i] > sum + nums[i]) ? nums[i] : sum + nums[i];
             
-            
-            sum = Math.max(sum, current);
+            // Elimination of Math.max for global max tracking
+            if (sum > max) {
+                max = sum;
+            }
         }
         
-        return sum;
+        return max;
     }
 }
