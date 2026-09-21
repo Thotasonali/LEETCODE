7a10a753e37f57aec6485ea5121a230c27ba0707
Sonali <86544294+Thotasonali@users.noreply.github.com>
2026-09-21 00:17:21 -0400

    Time: 13 ms (49.34%) | Memory: 22.2 MB (46.24%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,16 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
       
-        l=len(strs)
-        if(l==0):
-            return []
-        else:
-            group=defaultdict(list)
-            for s in strs:
-                so= "".join(sorted(s))
-                if so in group:
-                    group[so].append(s)
-                else:
-                    group[so].append(s)
+        
+        group=defaultdict(list)
+        for s in strs:
+            so= "".join(sorted(s))
+            
+            group[so].append(s)
+           
         return list(group.values())
         
