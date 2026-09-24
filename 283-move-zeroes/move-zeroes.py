6bb4ed201530d14be0407466db9209ddfa9cdd82
Sonali <86544294+Thotasonali@users.noreply.github.com>
2026-09-24 00:32:27 -0400

    Time: 6 ms (44.29%) | Memory: 20.4 MB (90.32%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,13 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l=len(nums)
-        one=0
-        two=0
-        for two in range (l):
-            if(nums[two]!=0):
-                temp=nums[two]
-                nums[two]=nums[one]
-                nums[one]=temp
-                one+=1
-             
+        insert_pos = 0
+        
+        # Step 1: Move all non-zero elements to the front
+        for num in nums:
+            if num != 0:
+                nums[insert_pos] = num
+                insert_pos += 1
+                
+        # Step 2: Fill the remaining positions with zeroes
+        for i in range(insert_pos, len(nums)):
+            nums[i] = 0
